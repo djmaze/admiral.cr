4d205914db48984333da0debf6e816881aec74fd
Martin Honermeyer <maze@strahlungsfrei.de>
2019-11-03 15:47:04 +0100

    Fix tempfile usage in specs for Crystal >= 0.27.0

--- spec/admiral/command_spec.cr
+++ spec/admiral/command_spec.cr
@@ -1,11 +1,10 @@
 require "spec"
-require "tempfile"
 require "../fixtures/*"
 
 describe "commands" do
   context "when required" do
     it "should raise an error" do
-      Tempfile.open("test") do |io|
+      File.tempfile("test") do |io|
         RequiredArgumentCommand.run([] of String, error: io)
         io.rewind
         io.gets_to_end.should eq "Missing required attribute: <aa>".colorize(:red).to_s + "\n"
@@ -14,7 +13,7 @@ describe "commands" do
 
     context "when help is passed" do
       it "should not raise an error" do
-        Tempfile.open("test") do |io|
+        File.tempfile("test") do |io|
           RequiredArgumentCommand.run(["--help"] of String, output: io)
           io.rewind
           io.gets_to_end.includes?("HELP TEXT").should be_true
@@ -24,7 +23,7 @@ describe "commands" do
 
     context "when version is passed" do
       it "should not raise an error" do
-        Tempfile.open("test") do |io|
+        File.tempfile("test") do |io|
           RequiredArgumentCommand.run(["--version"] of String, output: io)
           io.rewind
           io.gets_to_end.includes?("1.0.0").should be_true
